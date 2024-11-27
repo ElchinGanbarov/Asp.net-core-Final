216ef14c5c7ba755b7535a9a0d18afb53be3efe2
Eldar <ehasanov059@gmail.com>
2024-11-27 21:00:28 +0400

    222

--- AspFinal/Repository/obj/Debug/netcoreapp3.1/Repository.AssemblyInfo.cs
+++ AspFinal/Repository/obj/Debug/netcoreapp3.1/Repository.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Repository")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d6cccb0d9e8b444dd3f2fa51d273ec1f1130f17d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Repository")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Repository")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
